561517016ca9bb2ebebbcef9902d61f8b808feb6
maslenj <james.maslen@tufts.edu>
2022-11-09 13:25:13 -0500

    update logic agent

--- leduc_logic_agent.py
+++ leduc_logic_agent.py
@@ -9,9 +9,16 @@ class LogicAgent(object):
     # testing policy
     @staticmethod
     def step(state):
-        print(state)
-        return np.random.choice(list(state['legal_actions'].keys()))
+        # raise if we have a kind and fold otherwise
+        if state['raw_obs']['hand'] == 'HK' or state['raw_obs']['hand'] == 'SK':
+            if 'raise' in state['raw_obs']['legal_actions']:
+                return state['raw_obs']['legal_actions'].index('raise')
+            else:
+                return state['raw_obs']['legal_actions'].index('call')
+        else:
+            return np.random.choice(list(state['legal_actions'].keys()))
 
     # training policy
     def eval_step(self, state):
+        # this agent does not do any learning
         return self.step(state), {}
--- main.py
+++ main.py
@@ -16,10 +16,7 @@ env.set_agents([
 # run 1000 simulations
 player_1_avg = 0
 player_2_avg = 0
-num_games = 1000
-
-env.run(is_training=False)
-exit()
+num_games = 10000
 
 for i in range(num_games):
     _, payoffs = env.run(is_training=False)
@@ -27,4 +24,4 @@ for i in range(num_games):
     player_2_avg += payoffs[1]
 player_1_avg /= num_games
 player_2_avg /= num_games
-print(f"after {num_games} games: \nplayer 1 averaged {player_1_avg} points \nplayer 2 averaged {player_2_avg} points")
+print(f"after {num_games} games: \nplayer 1 (random) averaged {player_1_avg} points \nplayer 2 (logic) averaged {player_2_avg} points")
