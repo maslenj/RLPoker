c1caa131708fa2727e6b6a3c24ae8e2e4e08c310
Robert Pitkin <robert.pitkin@tufts.edu>
2022-11-20 15:42:49 -0500

    Implement helper functions

--- games/holdem/holdem_game.py
+++ games/holdem/holdem_game.py
@@ -1,11 +1,15 @@
 import numpy as np
+import random
 
 
 class HoldemPoker:
-    DECK_CARDS = ['As', 'Ah', 'Ac', 'Ad']
+    SUITS = ['s', 'h', 'c', 'd']
+    RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
+    DECK_CARDS = [x + y for x in RANKS for y in SUITS]
 
-    def __init(self, bankrolls=(100, 100)):
+    def __init__(self, bankrolls=(100, 100)):
         self.num_players = 2
+        self.deck = self.DECK_CARDS.copy()
         self.community_cards = []
         self.round_number = 0
         self.round_history = ('r', 'r')
@@ -18,22 +22,37 @@ class HoldemPoker:
             } for i in range(self.num_players)
         ]
 
-    # todo:
+    # todo: Rob
     def is_game_over(self):
-        pass
+        return self.round_number == 4
 
     # todo: Jimmy
     def decide_winner(self):
         pass
 
-    # todo:
+    # todo: Rob
     def deal_card(self):
-        pass
+        card = random.choice(self.deck)
+        self.deck.remove(card)
+        return card
 
-    # todo:
+    # todo: Rob
     def input_state(self, state):
-        pass
+        self.community_cards, self.round_number, \
+          self.round_history, self.pot, \
+          self.calling_amount, self.players = state
 
-    # todo:
+    # todo: Rob
     def print_state(self):
-        pass
+        print("-----------------------------")
+        print("Community cards:", self.community_cards)
+        print("Round number:", self.round_number)
+        print("Round history:", self.round_history)
+        print("Pot:", self.pot)
+        print("Calling amount:", self.calling_amount)
+        for i, player in enumerate(self.players):
+            print("-----------------------------")
+            print(f"Curr Player: {i}")
+            print("Hand:", player['hand'])
+            print("Bankroll:", player['bankroll'])
+        print("-----------------------------")
