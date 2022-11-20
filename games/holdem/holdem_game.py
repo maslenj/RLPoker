bdaf3d9d8e6211d45c02acd8ff5d26eb1c4f1d97
Robert Pitkin <robert.pitkin@tufts.edu>
2022-11-20 16:41:10 -0500

    Initial game implementation

--- games/holdem/holdem_game.py
+++ games/holdem/holdem_game.py
@@ -1,28 +1,30 @@
 from pokerlib import HandParser
-from pokerlib.enums import Value, Suit
+from pokerlib.enums import Rank, Suit
+import random
 
 
 class HoldemPoker:
-    SUITS = ['s', 'h', 'c', 'd']
-    RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
-    DECK_CARDS = [x + y for x in RANKS for y in SUITS]
+    DECK = ['2s', '2h', '2c', '2d', '3s', '3h', '3c', '3d', '4s', '4h', '4c', '4d', '5s', '5h', '5c', '5d',
+            '6s', '6h', '6c', '6d', '7s', '7h', '7c', '7d', '8s', '8h', '8c', '8d', '9s', '9h', '9c', '9d',
+            'Ts', 'Th', 'Tc', 'Td', 'Js', 'Jh', 'Jc', 'Jd', 'Qs', 'Qh', 'Qc', 'Qd', 'Ks', 'Kh', 'Kc', 'Kd',
+            'As', 'Ah', 'Ac', 'Ad']
     PLAYER_0_WIN = 0
     PLAYER_1_WIN = 1
-    SPLIT_POT    = 2
+    SPLIT_POT = 2
     RankDict = {
-        'A': Value.ACE,
-        '2': Value.TWO,
-        '3': Value.THREE,
-        '4': Value.FOUR,
-        '5': Value.FIVE,
-        '6': Value.SIX,
-        '7': Value.SEVEN,
-        '8': Value.EIGHT,
-        '9': Value.NINE,
-        'T': Value.TEN,
-        'J': Value.JACK,
-        'Q': Value.QUEEN,
-        'K': Value.KING,
+        'A': Rank.ACE,
+        '2': Rank.TWO,
+        '3': Rank.THREE,
+        '4': Rank.FOUR,
+        '5': Rank.FIVE,
+        '6': Rank.SIX,
+        '7': Rank.SEVEN,
+        '8': Rank.EIGHT,
+        '9': Rank.NINE,
+        'T': Rank.TEN,
+        'J': Rank.JACK,
+        'Q': Rank.QUEEN,
+        'K': Rank.KING,
     }
     SuitDict = {
         's': Suit.SPADE,
@@ -33,18 +35,95 @@ class HoldemPoker:
 
     def __init__(self, bankrolls=(100, 100)):
         self.num_players = 2
-        self.deck = self.DECK_CARDS.copy()
+        self.deck = self.DECK.copy()
         self.community_cards = []
         self.round_number = 0
-        self.round_history = ('r', 'r')
+        self.bet_number = 0
         self.pot = 0
         self.calling_amount = 0
+        self.raise_amount = 5
+        self.start_player = 0
+        self.current_player = 0
+        self.num_raises = 0
         self.players = [
             {
                 'hand': [],
                 'bankroll': bankrolls[i]
             } for i in range(self.num_players)
         ]
+        self.new_game()
+
+    def get_legal_actions(self):
+        actions = ['call', 'raise', 'fold']
+        if self.num_raises > 1:
+            actions.remove('raise')
+        return actions
+
+    def game_step(self):
+        if self.is_game_over():
+            result = self.decide_winner()
+            if result == HoldemPoker.PLAYER_0_WIN:
+                self.players[0]['bankroll'] += self.pot
+            elif result == HoldemPoker.PLAYER_1_WIN:
+                self.players[1]['bankroll'] += self.pot
+            else:
+                self.players[0]['bankroll'] += self.pot / 2
+                self.players[1]['bankroll'] += self.pot / 2
+            self.new_game()
+            return
+        if self.round_number == 0:
+            for p in self.players:
+                for i in range(2):
+                    p['hand'].append(self.deal_card())
+        elif self.round_number == 1:
+            for i in range(3):
+                self.community_cards.append(self.deal_card())
+        else:
+            self.community_cards.append(self.deal_card())
+
+    def make_move(self, action):
+        if action not in self.get_legal_actions():
+            print("Illegal action")
+            return
+        if action == "call":
+            self.pot += self.calling_amount
+            self.players[self.current_player]['bankroll'] -= self.calling_amount
+            self.calling_amount = 0
+            self.current_player = (self.current_player + 1) % 2
+            if self.bet_number > 0:
+                self.round_number += 1
+                self.game_step()
+                self.bet_number = 0
+                return
+            self.bet_number += 1
+        elif action == 'fold':
+            self.players[(self.current_player + 1) % 2]['bankroll'] += self.pot
+            self.pot = 0
+            self.new_game()
+            return
+        else:
+            # Raise
+            self.pot += self.raise_amount
+            self.calling_amount = self.raise_amount
+            self.players[self.current_player]['bankroll'] -= self.calling_amount
+            self.bet_number += 1
+            self.current_player = (self.current_player + 1) % 2
+            self.num_raises += 1
+
+    def new_game(self):
+        self.deck = self.DECK.copy()
+        self.community_cards = []
+        self.round_number = 0
+        self.start_player = (self.start_player + 1) % 2
+        self.bet_number = 0
+        self.pot = 2 * self.raise_amount
+        self.calling_amount = 0
+        self.current_player = self.start_player
+        self.num_raises = 0
+        for p in self.players:
+            p['hand'] = []
+            p['bankroll'] -= self.raise_amount
+        self.game_step()
 
     # todo: Rob
     def is_game_over(self):
@@ -89,17 +168,16 @@ class HoldemPoker:
         return card
 
     # todo: Rob
-    def input_state(self, state):
-        self.community_cards, self.round_number, \
-          self.round_history, self.pot, \
-          self.calling_amount, self.players = state
+    # def input_state(self, state):
+    #     self.community_cards, self.round_number, \
+    #     self.round_history, self.pot, \
+    #     self.calling_amount, self.players = state
 
     # todo: Rob
     def print_state(self):
         print("-----------------------------")
         print("Community cards:", self.community_cards)
         print("Round number:", self.round_number)
-        print("Round history:", self.round_history)
         print("Pot:", self.pot)
         print("Calling amount:", self.calling_amount)
         for i, player in enumerate(self.players):
@@ -108,3 +186,15 @@ class HoldemPoker:
             print("Hand:", player['hand'])
             print("Bankroll:", player['bankroll'])
         print("-----------------------------")
+
+
+def main():
+    P = HoldemPoker()
+    while True:
+        P.print_state()
+        action = input()
+        P.make_move(action)
+
+
+if __name__ == '__main__':
+    main()
