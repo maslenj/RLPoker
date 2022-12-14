0215038c8e5083c594bf57a128676a9ae091612c
maslenj <james.maslen@tufts.edu>
2022-12-13 22:50:21 -0500

    add results

--- tests/run_experiment.py
+++ tests/run_experiment.py
@@ -2,12 +2,14 @@ from games.holdem.holdem_game import HoldemPoker
 from agents.holdem.holdem_random_agent import HoldemRandomAgent
 from agents.holdem.holdem_mcts_agent import HoldemMCTSAgent
 from agents.holdem.holdem_dqn_agent import HoldemDQNAgent
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def run_experiment(num_games, agents):
     game = HoldemPoker(bankrolls=(100, 100))
     for game_number in range(num_games):
-        print(game_number)
+        # print(game_number)
         player_bankrupt = False
         for p in game.players:
             if p["bankroll"] < game.raise_amount:
@@ -19,15 +21,69 @@ def run_experiment(num_games, agents):
         while not game.is_game_over():
             action = agents[game.current_player].get_action(game)
             game.make_move(action)
-    print(f"player 0 result: {game.players[0]['bankroll']}")
-    print(f"player 1 result: {game.players[1]['bankroll']}")
+    return game.players[0]['bankroll'], game.players[1]['bankroll']
 
 
 def main():
-    agents1 = [HoldemMCTSAgent(1000), HoldemDQNAgent()]
-    agents2 = [HoldemMCTSAgent(1000), HoldemRandomAgent()]
-    run_experiment(50, agents1)
-    run_experiment(50, agents2)
+    names = [
+        "MCTS w/ DQN",
+        "MCTS w/out DQN",
+        "DQN",
+        "Random"
+    ]
+    agents = [
+        HoldemMCTSAgent(500, use_dqn=True),
+        HoldemMCTSAgent(500, use_dqn=False),
+        HoldemDQNAgent(),
+        HoldemRandomAgent(),
+    ]
+    results = np.array([[0.5 for _ in range(len(agents))] for _ in range(len(agents))])
+
+    # run grid tournament
+    for i in range(len(agents)):
+        for j in range(i + 1, len(agents)):
+            print(f"running {names[i]} vs {names[j]}")
+            num_wins = 0
+            games_to_play = 50
+            for g in range(games_to_play):
+                print(f"{g + 1}/{games_to_play}")
+                i_br, j_br = run_experiment(10, [agents[i], agents[j]])
+                if i_br > j_br:
+                    num_wins += 1
+            results[i][j] = num_wins / games_to_play
+            results[j][i] = 1 - results[i][j]
+
+    for row in results:
+        for col in row:
+            print(col, end=', ')
+        print()
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(results)
+
+    # Show all ticks and label them with the respective list entries
+    ax.set_xticks(np.arange(len(names)), labels=names)
+    ax.set_yticks(np.arange(len(names)), labels=names)
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    for i in range(len(names)):
+        for j in range(len(names)):
+            text = ax.text(j, i, round(results[i, j], 2),
+                           ha="center", va="center", color="w")
+
+    ax.set_title("Results of Tournament by Win Percentage")
+    fig.tight_layout()
+    plt.show()
+
+    print("done")
+    # agents1 = [HoldemMCTSAgent(1000), HoldemDQNAgent()]
+    # agents2 = [HoldemMCTSAgent(1000), HoldemRandomAgent()]
+    # run_experiment(50, agents1)
+    # run_experiment(50, agents2)
 
 
 if __name__ == '__main__':
